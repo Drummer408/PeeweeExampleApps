6331a6ae137981273658069ef536c227b3539285
Chris Khedoo <ckhedoo@gmail.com>
2017-11-17 04:47:21 +0000

    Added functionality for keeping track of the current user logged in.

--- CMS/app.py
+++ CMS/app.py
@@ -4,6 +4,8 @@ import getpass
 
 from models import db, User
 
+active_user = None
+
 def initialize():
     db.connect()
     db.create_tables([User], safe = True)
@@ -12,13 +14,14 @@ def login_loop():
     while True:
         username = input('Enter username: ')
         password = getpass.getpass('Enter password: ')
-        if validate_credentials(username, password):
+        user_content = User.get_user(username)
+        if validate_credentials(user_content, password):
             break
         print('\nInvalid username or password. Please try again.\n')
+    active_user = user_content
 
-def validate_credentials(username, password):
-    user = User.get_user(username)
-    return user != None and user.password == password
+def validate_credentials(user_content, password):
+    return user_content != None and user_content.password == password
 
 def menu_loop():
     pass
